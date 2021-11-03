5c5306cd6394a754dc1cf3943928369d9302e8c0
Alec Williams <alec.b.williams@wsu.edu>
2021-11-03 16:30:45 -0700

    Communication testing

---
+++ src/MyComponent.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactComponent from "react";
+
+function MyComponent(props) {
+    return (
+      <button>{props.text}</button>
+    );
+}
+
+export default MyComponent;
