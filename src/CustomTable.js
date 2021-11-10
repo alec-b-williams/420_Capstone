2cdc260074fd9b787d59dc4d1a170784674766d1
NamiSaya <73250718+NamiSaya@users.noreply.github.com>
2021-11-09 17:00:30 -0800

    Table Display Up
    - Data is displayed in a table
    - Font Change
    - Button Attribute changed

--- src/CustomTable.js
+++ src/CustomTable.js
@@ -1,19 +1,15 @@
 import React from "react";
-
+import './CustomTable.css';
 
 function CustomTableEntry(props) {
     return (
+      <section>
       <div className='tableEntry' >
-        <div className='orderCustName'>
-          {props.name}
-        </div>
-        <div className='orderDate'>
-          {props.date}
-        </div>
-        <div className='orderStatus'>
-          {props.status}
-        </div>
+        <div className='holder orderCustName'> {props.name}</div>
+        <div className='holder orderDate'> {props.date}</div>
+        <div className='holder orderStatus'> {props.status}</div>
       </div>
+      </section>
     );
 }
 
