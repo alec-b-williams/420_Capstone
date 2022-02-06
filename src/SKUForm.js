8ff03031283e0221a539292a61ecdd0ba70cb8c1
NamiSaya <73250718+NamiSaya@users.noreply.github.com>
2022-02-05 17:11:36 -0800

    More fields
    - More fields
    - Reorganized form

--- src/SKUForm.js
+++ src/SKUForm.js
@@ -40,33 +40,55 @@ function SKUForm(props) {
       <div className="pop-up">
           <div className="popup-inner">
               <form id="sku-selection"> 
+              
                 <div className="userName">
-                  Name: 
-                  <input type="text" id="NameField" placeholder="Enter your Name"></input>
+                  Name 
+                  <input type="text" id="NameField" placeholder=""></input>
                 </div>
+
                 <div className="address">
-                  Address:
-                  <input type="text" id="AddressField" placeholder="Enter your Address"></input>
+                  Address 
+                  <input type="text" id="AddressField" placeholder=""></input>
                 </div>
+
+                <div className="companyName">
+                  Company
+                  <input type="text" id="Company" placeholder=""></input>
+                </div>
+
+                <div className="city">
+                  City
+                  <input type="text" id="cityLoc" placeholder=""></input>
+                </div>
+
+                <div className="postalCode">
+                  Postal/Zip
+                  <input type="text" id="postal" placeholder=""></input>
+                </div>
+
                 <div className="fileLink">
-                  File URL:
-                  <input type="text" id="FileURL" placeholder="Enter File URL"></input>
+                  File URL
+                  <input type="text" id="FileURL" placeholder=""></input>
                 </div>
+
                 <div className="skuSelect">
-                  SKU: 
+                  SKU
                   <select name="SKU" onChange={(o) => props.setSKU(o.target.value)} id="SKUCombo">
                     {SKUOptions}
                   </select>
                 </div>
+
                 <div className="compSelect">
-                  Component: 
+                  Component
                   <select name="Component" onChange={(o) => props.setComponent(o.target.value)} id="ComponentCombo">
                     {ComponentOptions}
                   </select>
+
                 </div>
               </form>
               {props.children}
           </div>
+
       </div>
     </section>
   ) : "";
