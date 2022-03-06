48c7f66a8583a862dc4d658858115347ededf245
alec-b-williams <alec.b.williams@wsu.edu>
2022-03-06 14:37:36 -0800

    Revert "Working on new table for View Orders"
    This reverts commit f93fa16f33320914fb619c0dc0f0bb394e78c213.

--- src/ViewOrderPage.js
+++ src/ViewOrderPage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import CustomTableEntry from './CustomTable';
 import TableButton from "./TableButton.js";
-import OrdersTable from './OrdersTable.js';
 import "./styles/ViewWrapper.css"
 
 export default class ViewOrderPage extends React.Component {
@@ -14,21 +13,26 @@ export default class ViewOrderPage extends React.Component {
     this.fetchOrders = this.fetchOrders.bind(this);
   }
 
-  componentDidMount() {
-    this.fetchOrders();
-  }
-
   render() {
-    /*tableEntries.push(<CustomTableEntry
+
+    let tableEntries = [];
+    
+    tableEntries.push(<CustomTableEntry
     name={"Name"}
     date={"Date"}
-    status={"Status"}/>)*/
+    status={"Status"}/>)
 
     //do a for-loop here to generate a list of CustomTableEntries
     //for (var i=0; i < this.state.orders.data.length; i++) {
     this.state.orders?.data.forEach( order => {
-      console.log(order.orderData)
-    });
+      console.log(order._id)
+      tableEntries.push(<CustomTableEntry 
+        key={order._id}
+        name={order.orderData.customerName}
+        date={order.orderData.date} 
+        status={order.orderData.status}/>)
+      }
+    );
 
     return (
       <div id="ViewWrapper">
@@ -36,20 +40,14 @@ export default class ViewOrderPage extends React.Component {
           <TableButton text="Get Orders" onClick={this.fetchOrders} />
         </div>
 
-        {/*<div id="SearchWrapper">
-          <input type="text" id="SearchField" maxLength="20" placeholder="" onChange={(o) => props.setAddress(o.target.value)}></input>
-        </div>*/}
-
-        {/*<div className="CustomTable">
+        <div className="CustomTable">
           {tableEntries} 
-        </div>*/}
-
-        <OrdersTable orders={this.state.orders}/>
+        </div>
       </div>
     );
   }
 
-  fetchOrders = () => {
+  fetchOrders() {
     var data;
 
     fetch("/orders", {
